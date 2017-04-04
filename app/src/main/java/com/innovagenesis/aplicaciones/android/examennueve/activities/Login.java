3627dbb825693dcdd01fd34c7c2c795fbe14561d
AlexisRoj <alexisroj2@gmail.com>
2017-04-04 07:02:17 -0500

    Funcionando Login

--- app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/activities/Login.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/activities/Login.java
@@ -1,36 +1,123 @@
 package com.innovagenesis.aplicaciones.android.examennueve.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.innovagenesis.aplicaciones.android.examennueve.DiccionarioDatos;
 import com.innovagenesis.aplicaciones.android.examennueve.LoginDialogo;
 import com.innovagenesis.aplicaciones.android.examennueve.R;
+import com.innovagenesis.aplicaciones.android.examennueve.asynctask.login.ConsultarLoginAsync;
 
-public class Login extends AppCompatActivity implements LoginDialogo.DatosHacerLogin {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Login extends AppCompatActivity implements LoginDialogo.DatosHacerLogin,
+        ConsultarLoginAsync.ValidarLoginUsuario {
 
     private SharedPreferences preferences;
+    private Boolean recordarLogin = false;
+
+    private String usuario, contrasena;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        preferences = getSharedPreferences(DiccionarioDatos.PREFERENCE_LOGIN,MODE_PRIVATE);
+        preferences = getSharedPreferences(DiccionarioDatos.PREFERENCE_LOGIN, MODE_PRIVATE);
 
+
+        usuario = preferences.getString(DiccionarioDatos.nombreUsuario, null);
+        contrasena = preferences.getString(DiccionarioDatos.passUsuario, null);
+
+        if (usuario != null && contrasena != null)
+            mLoginUsuario(usuario, contrasena);
 
         LoginDialogo loginDialogo = new LoginDialogo();
-        loginDialogo.setCancelable(false);
-        loginDialogo.show(getSupportFragmentManager(),LoginDialogo.TAG);
+        loginDialogo.setCancelable(false); // Evita que se cierre el dialogo
+        loginDialogo.show(getSupportFragmentManager(), LoginDialogo.TAG);
 
         this.setTitle("Ingreso al sistema");
+
+        if (!mValidarConexion()) { // Valida si existe conexion
+            Toast.makeText(this, "Error al establecef conexion", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
     @Override
     public void hacerLogin(String usuario, String contrasena, Boolean recordar) {
+        // envia a consultar los datos atrapados del login y sus campos
+
+        recordarLogin = recordar;
+
+        SharedPreferences.Editor editor = preferences.edit();
+
+        if (recordar) {
+            //Guarda las preferencias
+            editor.putString(DiccionarioDatos.nombreUsuario, usuario);
+            editor.putString(DiccionarioDatos.passUsuario, contrasena);
+            editor.apply();
+        } else {
+            //Borra las preferencias
+            editor.remove(DiccionarioDatos.nombreUsuario);
+            editor.remove(DiccionarioDatos.passUsuario);
+            editor.apply();
+        }
 
 
+        mLoginUsuario(usuario, contrasena);
+    }
+
+    /**
+     * Inicia el proceso de login
+     */
+    private void mLoginUsuario(String usuario, String contrasena) {
+        try {
+            new ConsultarLoginAsync(this).execute(new URL(DiccionarioDatos.URL_SERVICIO_USUARIO +
+                    usuario + "/" + contrasena));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Interface que permite el ingreso al sistema
+     */
+    @Override
+    public void LoginUsuario(String userName, String pass, int rol) {
+        // si el usuario existe permite la carga de la otra activity
+
+        if (rol > 0)
+            if (rol == 3 || rol == 1)
+                //rol 3 activa al profesor
+                mCargarActivity();
+            else
+                Toast.makeText
+                        (this, getString(R.string.mensajePrivilegios), Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Valida si existe conexion
+     */
+    private boolean mValidarConexion() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    /**
+     * Metodo encargado de cargar la actividad
+     */
+    private void mCargarActivity() {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/asynctask/login/ConsultarLoginAsync.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/asynctask/login/ConsultarLoginAsync.java
@@ -1,27 +1,102 @@
 package com.innovagenesis.aplicaciones.android.examennueve.asynctask.login;
 
-import android.net.Uri;
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
-
+import android.widget.Toast;
+import com.innovagenesis.aplicaciones.android.examennueve.R;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  * Clase encargada de realizar la consulta a la base de datos
  * Created by alexi on 02/04/2017.
  */
 
-public class ConsultarLoginAsync extends AsyncTask <Uri, Integer, String> {
+public class ConsultarLoginAsync extends AsyncTask<URL, Integer, String> {
 
+    private String username = "";
+    private String pass = "";
+    private int rol = 0;
 
-    HttpURLConnection connection = null;
+    private Activity activity;
+    private ProgressDialog progressDialog;
 
     @Override
-    protected String doInBackground(Uri... params) {
+    protected String doInBackground(URL... params) {
+
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection) params[0].openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            return reader.readLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            assert connection != null;
+            connection.disconnect();
+        }
         return null;
+    }
 
-
+    public interface ValidarLoginUsuario {
+        void LoginUsuario(String userName, String pass, int rol);
     }
 
 
+    private ValidarLoginUsuario listener;
 
+    public ConsultarLoginAsync(Activity activity) {
+        this.activity = activity;
+        progressDialog = new ProgressDialog(activity);
+
+        try {
+            listener = (ValidarLoginUsuario) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("La activity no implementa la interfaz consultar usuario");
+        }
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog.show();
+        progressDialog.setMessage(activity.getString(R.string.procesando));
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
+
+        if (s != null) {
+            try {
+                /**
+                 * Es necesario recorrer toda el objecto para extraer la informacion necesaria
+                 * */
+                JSONObject jsonObject = new JSONObject(s);
+                for (int i = 0; i < jsonObject.length(); i++) {
+                    username = jsonObject.getString("nom_usuario");
+                    pass = jsonObject.getString("pass_usuario");
+                    rol = jsonObject.getInt("rol_user");
+                }
+                listener.LoginUsuario(username, pass, rol);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else
+            Toast.makeText(activity, R.string.contrasenaNoValida,Toast.LENGTH_SHORT).show();
+    }
 }
