608697ba9523ff3ff2ce3c46ebb7484d9b4b7a80
AlexisRoj <alexisroj2@gmail.com>
2017-04-10 13:24:54 -0500

    Funcionando Json para rellenar los spinner, creando interface para guardar tareas, en la base de datos

--- app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/dialogos/DialogoAgregarTareas.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/dialogos/DialogoAgregarTareas.java
@@ -3,25 +3,28 @@ package com.innovagenesis.aplicaciones.android.examennueve.dialogos;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-
+import android.widget.TextView;
 import com.innovagenesis.aplicaciones.android.examennueve.R;
 import com.innovagenesis.aplicaciones.android.examennueve.instancias.Tareas;
-
 import org.json.JSONArray;
 import org.json.JSONException;
-
-
 import java.util.ArrayList;
+
 
 /**
  * Dialogo encargado de realizar el ingreso de tareas
@@ -34,8 +37,16 @@ public class DialogoAgregarTareas extends DialogFragment {
     public static String jsonAsigna = "json_asigna";
     public static String jsonEstu = "json_estudiante";
     public String nombreTarea = "nombre_tarea";
-    private String deserializaAsinga, deserializaEstud;
     private static Bundle argumentos;
+
+    /**
+     * Spinners y Json
+     */
+
+    private ArrayList<String> nombreAsigna;
+    private ArrayList<String> codAsigna;
+    private ArrayList<String> nombreEstu;
+    private ArrayList<String> codEstu;
 
 
     private interface DatosGuardarTarea {
@@ -43,13 +54,16 @@ public class DialogoAgregarTareas extends DialogFragment {
     }
 
 
+    /**
+     * Recibe dos json los cuales despues los deserializa para llenar los spinner
+     */
     public static DialogoAgregarTareas newInstance(String jsonEstudiante, String jsonAsignatura) {
 
         DialogoAgregarTareas fragment = new DialogoAgregarTareas();
 
         argumentos = new Bundle();
         argumentos.putString(jsonAsigna, jsonAsignatura);
-        argumentos.putString(jsonEstu,jsonEstudiante);
+        argumentos.putString(jsonEstu, jsonEstudiante);
         fragment.setArguments(argumentos);
 
         return fragment;
@@ -74,15 +88,15 @@ public class DialogoAgregarTareas extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        @SuppressLint("InflateParams")
+        @SuppressLint("InflateParams") final
         View view = LayoutInflater.from(getContext())
                 .inflate(R.layout.dialogo_agregar_tarea, null);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setView(view);
 
-        deserializaAsinga = argumentos.getString(jsonAsigna);
-        deserializaEstud = argumentos.getString(jsonEstu);
+        String deserializaAsinga = argumentos.getString(jsonAsigna);
+        String deserializaEstud = argumentos.getString(jsonEstu);
 
         try {
             /*
@@ -91,28 +105,45 @@ public class DialogoAgregarTareas extends DialogFragment {
              * otro es para guardar los datos, es el codigo
              * */
             JSONArray jsonAsina = new JSONArray(deserializaAsinga);
-            ArrayList<String> nombreAsigna = new ArrayList<>();
-            ArrayList<Integer> codAsigna = new ArrayList<>();
+
+            nombreAsigna = new ArrayList<>();
+            codAsigna = new ArrayList<>();
+
+            nombreAsigna.add(getString(R.string.spinerMateria));
+            /**
+             *
+             * TODO recordar quitar uno a el contador de codigo, para que guarde el codigo correcto
+             *
+             * */
 
             for (int i = 0; i < jsonAsina.length(); i++) {
 
                 nombreAsigna.add(jsonAsina.getJSONObject(i).getString("nom_asigna"));
-                codAsigna.add(Integer.valueOf(jsonAsina.getJSONObject(i).getString("id_asigna")));
+                codAsigna.add(jsonAsina.getJSONObject(i).getString("id_asigna"));
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
 
+
         try {
             JSONArray jsonEstu = new JSONArray(deserializaEstud);
-            ArrayList<String> nombreEstu = new ArrayList<>();
-            ArrayList<Integer> codEstu = new ArrayList<>();
 
-            for (int i = 0; i<jsonEstu.length(); i++){
+            nombreEstu = new ArrayList<>();
+            codEstu = new ArrayList<>();
+
+            nombreEstu.add(getString(R.string.selectEstudiante));
+            /**
+             *
+             * TODO recordar quitar uno a el contador de codigo, para que guarde el codigo correcto
+             *
+             * */
+
+            for (int i = 0; i < jsonEstu.length(); i++) {
 
                 nombreEstu.add(jsonEstu.getJSONObject(i).getString("nom_usuario"));
-                codEstu.add(Integer.valueOf(jsonEstu.getJSONObject(i).getString("ced_usuario")));
+                codEstu.add(jsonEstu.getJSONObject(i).getString("ced_usuario"));
 
             }
         } catch (JSONException e) {
@@ -131,6 +162,88 @@ public class DialogoAgregarTareas extends DialogFragment {
 
         Button btnAgregarTarea = (Button) view.findViewById(R.id.btn_ingresar_tarea);
         Button btnCancelarTarea = (Button) view.findViewById(R.id.btn_cancelar_tarea);
+
+
+        /* Sesión de los spinner materia
+        * Es llenado con el json y se le agrega un elemento arriba para que haga el hint
+        * */
+
+        final ArrayAdapter<String> stringArrayAdapterMateria = new ArrayAdapter<String>
+                (getActivity(), R.layout.spinner_item, nombreAsigna) {
+
+            @Override
+            public boolean isEnabled(int position) {
+                //Desabilita el primer elemento de la lista del spinner
+                return position != 0;
+            }
+
+            @Override
+            public View getDropDownView
+                    (int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                View view1 = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view1;
+
+                if (position == 0) {
+                    /** Funciona para efecto visual de un elemento del spinner*/
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view1;
+            }
+        };
+
+
+        final String[] codAsignatura = new String[1];
+
+        spinnerAsignatura.setAdapter(stringArrayAdapterMateria);
+        spinnerAsignatura.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Se corrije solo dado que se le envia la misma posicion al arreglo,
+                // como si tuviera otro elemento incluido
+                codAsignatura[0] = codAsigna.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+        /* Spinner Usuario o Estudiante*/
+
+        ArrayAdapter<String> stringArrayAdapterEstu = new ArrayAdapter<String>
+                (getActivity(),R.layout.spinner_item,nombreEstu){
+
+            @Override
+            public boolean isEnabled(int position) {
+                return position != 0;
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                View view1 =  super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView)view1;
+
+                if (position == 0) {
+                    /** Funciona para efecto visual de un elemento del spinner*/
+                    tv.setTextColor(Color.GRAY);
+                } else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view1;
+            }
+        };
+
+        spinnerEstudiante.setAdapter(stringArrayAdapterEstu);
+
+
+        /* Seccion de los botones del dialgo */
 
         btnAgregarTarea.setOnClickListener(new View.OnClickListener() {
             @Override
