efbb7e073871645c5d18990d3230c1b603d4a67e
AlexisRoj <alexisroj2@gmail.com>
2017-04-16 02:33:03 -0500

    Agregando tareas ok

--- app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/adapters/RecyclerViewAdapterTarea.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examennueve/adapters/RecyclerViewAdapterTarea.java
@@ -1,7 +1,9 @@
 package com.innovagenesis.aplicaciones.android.examennueve.adapters;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +11,11 @@ import android.view.ViewGroup;
 
 import com.innovagenesis.aplicaciones.android.examennueve.DiccionarioDatos;
 import com.innovagenesis.aplicaciones.android.examennueve.R;
+import com.innovagenesis.aplicaciones.android.examennueve.asynctask.BorrarTareaAsyncTask;
 import com.innovagenesis.aplicaciones.android.examennueve.instancias.Tareas;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
@@ -27,6 +32,10 @@ public class RecyclerViewAdapterTarea extends RecyclerView.Adapter<RecyclerViewH
     private List<Tareas> lista = Collections.emptyList();
     private LayoutInflater inflater;
     private Activity activity;
+
+    private String nomTarea, nomEstuTarea, nomAsignaTarea;
+    private int notaTarea,idTarea;
+
 
     public interface mEditarElementoRecycler{
         void EditarElementoRecycler(Bundle bundle);
@@ -55,7 +64,7 @@ public class RecyclerViewAdapterTarea extends RecyclerView.Adapter<RecyclerViewH
     }
 
     @Override
-    public void onBindViewHolder(RecyclerViewHolderTarea holder, int position) {
+    public void onBindViewHolder(final RecyclerViewHolderTarea holder, int position) {
 
         final Tareas current = lista.get(position);
         final int tareaBorrar = current.idTarea;
@@ -66,12 +75,10 @@ public class RecyclerViewAdapterTarea extends RecyclerView.Adapter<RecyclerViewH
         holder.nota_tarea_item.setText(String.valueOf(current.notaTarea));
 
 
+
         holder.imgEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String nomTarea, nomEstuTarea, nomAsignaTarea;
-                int notaTarea,idTarea;
 
                 idTarea = current.idTarea;
                 nomTarea = current.nomTarea;
@@ -95,6 +102,9 @@ public class RecyclerViewAdapterTarea extends RecyclerView.Adapter<RecyclerViewH
             @Override
             public void onClick(View v) {
 
+                //idTarea = current.idTarea;
+                mElimarTarea(holder,tareaBorrar).show();
+
             }
         });
     }
@@ -102,5 +112,47 @@ public class RecyclerViewAdapterTarea extends RecyclerView.Adapter<RecyclerViewH
     @Override
     public int getItemCount() {
         return lista.size();
+    }
+
+    public AlertDialog mElimarTarea(final RecyclerViewHolderTarea holder, final int idTareaBorrar) {
+
+        /** Dialogo que borra la cuenta de usuario en la que se encuentra*/
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+
+        builder.setTitle(R.string.eliminarTarea)
+                .setMessage(R.string.borrarTarea)
+                .setPositiveButton(R.string.aceptar,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int item = holder.getAdapterPosition();
+                                try {
+                                    /** Remueve el donante de la base de datos*/
+                                    new BorrarTareaAsyncTask(activity).execute(
+                                            new URL(DiccionarioDatos.URL_SERVICIO_TAREA
+                                                    + idTareaBorrar));
+                                    /** Remuelve el elemento del recyclerView*/
+                                    mRemoverTareaRecycler(item);
+                                } catch (MalformedURLException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        })
+                .setNegativeButton(activity.getString(R.string.cancelar),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+        return builder.create();
+    }
+
+    private void mRemoverTareaRecycler(int item) {
+        //Elimina los items del recyclerView
+        lista.remove(item);
+        notifyItemRemoved(item);
+        notifyItemRangeChanged(item, lista.size());
     }
 }
